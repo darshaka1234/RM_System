b1cb18b5e1ae86b40fb68c2e31b641333a1ac4fd
darshaka1234 <darshakasrimal1234@gmail.com>
2023-09-12 02:55:54 +0530

    Cors isse resolved

--- RM_System/Program.cs
+++ RM_System/Program.cs
@@ -33,6 +33,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
